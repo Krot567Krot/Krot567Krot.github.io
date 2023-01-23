b32bea533fc6ce8e17d9f03613ac3f27b7917441
Krot567Krot <112086036+Krot567Krot@users.noreply.github.com>
2023-01-23 18:45:10 +0300

    Delete src directory

--- src/src/js/maginific.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('.info-iconparent').magnificPopup({ type: 'image' });
-});
-
-$(document).ready(function () {
-    $('.card-icon-img').magnificPopup({ type: 'image' });
-});
